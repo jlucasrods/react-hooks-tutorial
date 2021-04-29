1f131e52b74846d6e3a023410b49e776e9ef5453
jlucasrods <jlucasrods@gmail.com>
2021-04-29 01:19:04 -0300

    add effect to fetch repos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useState, useEffect } from 'react';
+
 export default function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    async function fetchRepos() {
+      const response = await fetch('https://api.github.com/users/jlucasrods/repos');
+      setRepos(await response.json());
+    }
+    fetchRepos();
+  }, []);
+
   return (
-    <h1>Hello World</h1>
+    <ul>
+      {repos.map(repo => (
+        <li key={repo.id}>{repo.name}</li>
+      ))}
+    </ul>
   );
 }
